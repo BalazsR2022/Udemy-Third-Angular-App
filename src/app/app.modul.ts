4bfbdbfb4e52e69c6b579621146226cebc3c1c2c
Balázs Réka <balazsreka@ktch.hu>
2024-09-20 21:20:42 +0200

    ready

--- src/app/app.modul.ts
+++ src/app/app.modul.ts
@@ -1,23 +1,18 @@
+// 
 import { NgModule } from "@angular/core";
-import {FormsModule} from '@angular/forms';
-
+import { BrowserModule } from "@angular/platform-browser"; // Hiányzó import
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { UserInputComponent } from "./user-input/user-input.component";
 import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
-import { BrowserModule } from "@angular/platform-browser";
-
+import { UserinputModule } from "./user-input/user-input.module";
 
 @NgModule({
     declarations: [
         AppComponent, 
         HeaderComponent,
-        UserInputComponent, 
         InvestmentResultsComponent 
     ],
-        imports: [ BrowserModule,FormsModule],
-        bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, UserinputModule] // BrowserModule hozzáadva
 })
-export class AppModule{
-
-}
+export class AppModule { }
